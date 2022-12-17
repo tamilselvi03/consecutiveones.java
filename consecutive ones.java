ce64249b277198542b7ae738449c6e41d523b12c
Tamilselvi <112626100+tamilselvi03@users.noreply.github.com>
2022-12-17 21:45:35 +0530

    Create consecutive ones.java

---
+++ consecutive ones.java
@@ -0,0 +1,19 @@
+class Solution {
+    
+    
+     public int findMaxConsecutiveOnes(int[] nums) {  
+        int curCount = 0; int max = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 1){
+                curCount++;
+                max = max > curCount ? max : curCount;
+                
+            }else{
+              
+                curCount = 0;
+            }
+        }
+        return max;
+     }
+}
+   
